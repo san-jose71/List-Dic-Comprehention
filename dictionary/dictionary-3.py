6ce297667c77fa5743e4e639a60d76fc93f3db1a
san-jose71 <83045359+san-jose71@users.noreply.github.com>
2022-01-06 18:02:53 +1100

    Add files via upload

---
+++ dictionary/dictionary-3.py
@@ -0,0 +1,10 @@
+student_dic = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+
+for (key, value) in student_dic.items():
+    print(key)
+    print(value)
---
+++ dictionary/dictionary-4.py
@@ -0,0 +1,19 @@
+import pandas
+
+student_dic = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_data_frame = pandas.DataFrame(student_dic)
+print(student_data_frame)
+
+#Loop through a data frame
+
+# for (key, value) in student_data_frame.items():
+#     print(value)
+
+#Loop through rows of a data frame
+
+for (index, row) in student_data_frame.iterrows():
+    print(row)
